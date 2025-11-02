c59e200dc5b6c7b54087cc1f347560c1cf603e51
Bill Adams <jryvn@yahoo.com>
2025-11-01 21:51:51 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+string[] menu = new string[] { 
+    "eggs", "bacon", "spam", "sausage", "spam", "spam", "baked beans", "spam" };
+
+foreach (string item in menu)
+{
+    Console.WriteLine(item);
+}
